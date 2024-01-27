256b9db35fedccb40c7650cba48dea78dd5f6b59
Vini Bjazevich <vinibjazevich97@gmail.com>
2024-01-27 13:58:44 -0800

    fix: forcing route to be revalidate

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -1,10 +1,10 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
-export const dynamic = "force-dynamic";
-
 const prisma = new PrismaClient();
 
+export const revalidate = 1;
+export const dynamic = "force-dynamic";
 export async function GET() {
   let user = null;
 
