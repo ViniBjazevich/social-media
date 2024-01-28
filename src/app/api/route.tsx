8cbdf0e98364a277eb9c9a27493c83dfe6a873cc
Vini Bjazevich <vinibjazevich97@gmail.com>
2024-01-27 17:49:41 -0800

    feat: displaying user information

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
-
 export const revalidate = 1;
 export const dynamic = "force-dynamic";
 export async function GET() {
+  const prisma = new PrismaClient();
+
   let user = null;
 
   try {
